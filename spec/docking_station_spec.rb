6bb7c1187c3fd05f18b4702c27fdc9d0d0192f55
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-04-12 14:40:53 +0100

    Adds Bike test and 'working?' method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require "docking_station"
 
 describe DockingStation do
   it {respond_to(:release_bike)}
+
+  it "releases good bikes" do
+    bike = subject.release_bike
+    expect(bike.working).to eq true 
+  end
 end
